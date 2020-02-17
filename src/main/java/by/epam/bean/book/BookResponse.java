fc5bb5372d363ed4345edc41b43a32c0a779d8ff
OSmol <Olga_Smolyakova@outlook.com>
2020-02-17 20:07:05 +0300

    Update BookResponse.java

--- src/main/java/by/epam/bean/book/BookResponse.java
+++ src/main/java/by/epam/bean/book/BookResponse.java
@@ -3,7 +3,7 @@ package by.epam.bean.book;
 import by.epam.bean.Response;
 import by.epam.entity.Book;
 
-public class BookResponse extends Response {
+public class BookResponse extends Response {// аналогично соседнему классу в пакете
     private Book book = new Book();
 
     public Book getBook() {
